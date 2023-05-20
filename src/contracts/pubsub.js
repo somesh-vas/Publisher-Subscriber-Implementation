432db07cea4c5bf47b1c7a569c34bc3b41a1c9e3
somesh-vas <someshvas2@gmail.com>
2023-05-20 22:02:13 +0530

    project is ready to deploy

---
+++ src/contracts/pubsub.js
@@ -0,0 +1,266 @@
+import web3 from "./web3";
+
+const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_userID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicID",
+				"type": "uint256"
+			}
+		],
+		"name": "deleteTopic",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_pubid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_eventid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "hash",
+				"type": "string"
+			}
+		],
+		"name": "publishData",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_userID",
+				"type": "uint256"
+			}
+		],
+		"name": "registerPerson",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_userID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicID",
+				"type": "uint256"
+			}
+		],
+		"name": "registerTopic",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_subs",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_pub",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicid",
+				"type": "uint256"
+			}
+		],
+		"name": "removeSubs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_subs",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_pub",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicid",
+				"type": "uint256"
+			}
+		],
+		"name": "subscription",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "PersonDetails",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "pubData",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "eventID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "hash",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_pubid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_topicid",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_eventid",
+				"type": "uint256"
+			}
+		],
+		"name": "retHash",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "subData",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "topicRegList",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const addr = "0x0E0Ad5D3E43a2E7ECD0f7eC1717153d5C2db99f7"; // write the deployed address here
+
+export default new web3.eth.Contract(abi, addr);
---
+++ src/files/autosignup.js
@@ -0,0 +1,24 @@
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// // import pubsub from '../contracts/pubsub';
+// import "./login1.css"
+// import pubsub from '../contracts/pubsub';
+// export default function autosignup() {
+//         const res = await fetch("http://localhost:4000/signup", {
+//             method: "POST",
+//             body: JSON.stringify(userData),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         });
+
+//         const data = JSON.parse(await res.text());
+
+//         console.log(data.id);
+//         console.log(userData.addr);
+
+//         regPubSol(data.id, userData.addr);
+//         return(<></>);
+//     }
+
+    
